4cf57f75d729ed095e563f596e20ab46032892aa
hassanyakef <yhassan@vt.edu>
2020-11-30 15:16:39 -0500

    Add margin left for todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,7 +10,7 @@ const TodoItem = (props) => {
   return (
     <div className="input-group input-group-lg mb-3" style={todoStyle()}>
       <input style={{ width: '30px', height: '30px' }} type="checkbox"/>
-      <p className="lead">Do homework</p>
+      <p className="lead ml-2">Do homework</p>
       <button className="btn btn-danger ml-3 pb-3 pt-1" style={{ height: '2rem', borderRadius: '20%' }}>&times;</button>
     </div>
   );
